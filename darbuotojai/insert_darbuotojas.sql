6dd9c0c42108b2cf5c7d467f511a1d06083f04e3
reinikeda <reinikeda@gmail.com>
2023-01-17 08:17:53 +0200

    mano atskiras branch

--- darbuotojai/insert_darbuotojas.sql
+++ darbuotojai/insert_darbuotojas.sql
@@ -1,4 +1,4 @@
 -- SQLite
 INSERT INTO darbuotojai (vardas, pavarde, atlyginimas)
-VALUES ("Egle", "Smaguolyte", 3000);
+VALUES ("Egle", "Smaguolyte", 3000), ("Labas", "Rytas", 2000);
 SELECT * FROM darbuotojai;
---
+++ darbuotojai/pasizaidimas.sql
@@ -0,0 +1,10 @@
+-- INSERT INTO darbuotojai (vardas, pavarde, atlyginimas)
+-- VALUES ("Vardenis", "Pavardenis", 2200), ("Naminukas", "Bildukas", 955);
+
+-- DELETE FROM darbuotojai WHERE id = 3;
+
+-- UPDATE darbuotojai SET pavarde = 'Bismarkas' WHERE id = 5;
+
+-- SELECT * FROM darbuotojai ORDER BY atlyginimas DESC;
+
+SELECT * FROM darbuotojai ORDER BY atlyginimas;
